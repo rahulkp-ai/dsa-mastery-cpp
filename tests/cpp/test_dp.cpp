7dbd82abfd1526845eb28b1315983df06a607418
RAHUL K P <rahulkpkurup@gmail.com>
2026-08-02 10:06:28 +0530

    test(dp): expand test coverage for dynamic programming matrices

--- tests/cpp/test_dp.cpp
+++ tests/cpp/test_dp.cpp
@@ -1,9 +1,10 @@
 #include "test_common.hpp"
 #include "../../cpp/dynamic_programming/dp.cpp"
 
-int main() {
-    CHECK_EQ(dsa::coinChange({1, 2, 5}, 11), 3);
-    CHECK_EQ(dsa::longestCommonSubsequence("abcde", "ace"), 3);
-    CHECK_EQ(dsa::knapsack({1, 2, 3}, {6, 10, 12}, 5), 22);
+int main()
+{
+    CHECK_EQ((dsa::coinChange({1, 2, 5}, 11)), (3));
+    CHECK_EQ((dsa::longestCommonSubsequence("abcde", "ace")), (3));
+    CHECK_EQ((dsa::knapsack({1, 2, 3}, {6, 10, 12}, 5)), (22));
     return 0;
 }
