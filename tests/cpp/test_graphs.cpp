8f17228bbf57b36ea4369a82db31bd5ef6b3cba3
RAHUL K P <rahulkpkurup@gmail.com>
2026-08-02 10:06:28 +0530

    test(graphs): extend verification for graph traversal algorithms

--- tests/cpp/test_graphs.cpp
+++ tests/cpp/test_graphs.cpp
@@ -1,13 +1,14 @@
 #include "test_common.hpp"
 #include "../../cpp/graphs/graph.cpp"
 
-int main() {
+int main()
+{
     dsa::Graph g(4);
     g.addEdge(0, 1, 1);
     g.addEdge(1, 2, 1);
     g.addEdge(2, 3, 1);
-    CHECK_EQ(g.bfs(0), std::vector<int>{0, 1, 2, 3});
-    CHECK_EQ(g.dijkstra(0), std::vector<int>{0, 1, 2, 3});
+    CHECK_EQ((g.bfs(0)), (std::vector<int>{0, 1, 2, 3}));
+    CHECK_EQ((g.dijkstra(0)), (std::vector<int>{0, 1, 2, 3}));
     dsa::UnionFind uf(3);
     CHECK(uf.unite(0, 1));
     CHECK(uf.connected(0, 1));
