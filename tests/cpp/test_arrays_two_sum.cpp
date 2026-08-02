9c327bd9dc5c9f030fa2aaed5636090373b45135
RAHUL K P <rahulkpkurup@gmail.com>
2026-08-02 10:06:28 +0530

    test(arrays): add comprehensive test cases for array subproblems

--- tests/cpp/test_arrays_two_sum.cpp
+++ tests/cpp/test_arrays_two_sum.cpp
@@ -1,8 +1,9 @@
 #include "test_common.hpp"
 #include "../../cpp/arrays/two_sum.cpp"
 
-int main() {
-    CHECK_EQ(dsa::twoSum({2, 7, 11, 15}, 9), std::vector<int>{0, 1});
-    CHECK_EQ(dsa::twoSum({3, 2, 4}, 6), std::vector<int>{1, 2});
+int main()
+{
+    CHECK_EQ((dsa::twoSum({2, 7, 11, 15}, 9)), (std::vector<int>{0, 1}));
+    CHECK_EQ((dsa::twoSum({3, 2, 4}, 6)), (std::vector<int>{1, 2}));
     return 0;
 }
