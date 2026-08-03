3aed95f69f4f63728d79bbe5d1bd86a62cef0d81
RAHUL K P <rahulkpkurup@gmail.com>
2026-08-03 08:44:50 +0530

    test(queue): refresh verification suite for standard queue types

--- tests/cpp/test_queue.cpp
+++ tests/cpp/test_queue.cpp
@@ -1,7 +1,8 @@
 #include "test_common.hpp"
 #include "../../cpp/queues/queue.cpp"
 
-int main() {
-    CHECK_EQ(dsa::slidingWindowMax({1, 3, -1, -3, 5, 3, 6, 7}, 3), std::vector<int>{3, 3, 5, 5, 6, 7});
+int main()
+{
+    CHECK_EQ((dsa::slidingWindowMax({1, 3, -1, -3, 5, 3, 6, 7}, 3)), (std::vector<int>{3, 3, 5, 5, 6, 7}));
     return 0;
 }
